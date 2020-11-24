34b627a406b9decc90a784df05133812bb4c045e
Ahmed Khaled <ahmedkhaledabab@gmail.com>
2020-11-25 00:12:08 +0200

    created a submatrix

--- needleman_wunsh.py
+++ needleman_wunsh.py
@@ -1,4 +1,25 @@
+def getSubMatrix(match, mismatch, seq1, seq2):
+    H, W = len(seq1), len(seq2)
+    matrix = [[0 for i in range(W)] for y in range(H)]
+    
+    i = 0
+    while i < H:
+        j = 0
+        while j < W:
+            if seq1[i] == seq2[j]:
+                matrix[i][j] = match
+            else:
+                matrix[i][j] = mismatch
+            j += 1
+        i += 1
+
+    return matrix
+
+
+
 def alignUsingNW(seq1, seq2, match=1, mismatch=-1, gapPenalty=1):
+
+    subMatrix = getSubMatrix(match, mismatch, seq1, seq2)
 
     H, W = len(seq1) + 1, len(seq2) + 1
     scoringMatrix = [[0 for i in range(W)] for y in range(H)]
@@ -14,3 +35,5 @@ def alignUsingNW(seq1, seq2, match=1, mismatch=-1, gapPenalty=1):
                 scoringMatrix[i][j] = scoringMatrix[i][j - 1] - gapPenalty
             elif j == 0:
                 scoringMatrix[i][j] = scoringMatrix[i - 1][j] - gapPenalty
+            else:
+                pass
